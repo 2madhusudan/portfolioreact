eb1ecc5857bb31e0505508662e101848e56eebe1
2madhusudan <98676234+2madhusudan@users.noreply.github.com>
2023-10-14 23:00:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from "react";
 
 const App = () => {
   const portfolioData = {
-    name: "Prayush Giri",
-    reg:"21BCE7864",
-    place: "Lucknow , UP | India",
-    skills: ["Cloud Practitioner","Proficient in Java & C++", "Novice in Web Development(Mern)", "Intermediate in Database(mySQL & MongoDB)"],
+    name: "Madhusudan Das",
+    skills: ["Cloud Practitioner","Java & C++", "Web Development(Mern)", "Database(mySQL & MongoDB)","Flutter"],
     projects: [
       {
         title: "Farm2You | Agri-Tech Project",
@@ -16,7 +14,7 @@ const App = () => {
         description: "Integrating Raspberry Pi with high-definition cameras, live streaming capabilities, and sensor modules empowers users to engage with and explore their environment in real-time.",
       },
       {
-        title: "Prayush Giri | Portfolio Website",
+        title: "Madhusudan | Portfolio Website",
         description: "Explore my portfolio website, a dynamic showcase of my work powered by HTML, CSS, and JavaScript with the flexibility of Flexbox.",
       },
     ],
@@ -27,13 +25,11 @@ const App = () => {
       <div className="frame">
         <div className="header">
           <h1>{portfolioData.name}</h1>
-          <h2><u>Registration no:</u> {portfolioData.reg}</h2>
-          <h2><u>Postal Location:</u> {portfolioData.place}</h2>
         </div>
         <div className="body">
           <div className="about">
             <h3><u>Description about me:</u></h3>
-            <p>Hey! I am Prayush, 21 Years of age and currently pursuing my B.Tech Degree from VIT-AP in CSE-Core. I have been since the beginning of my time here on earth, fascinated by these machines<br/>that we call computers, and from understanding what the internet is and how it works to now learning to make my own websites is a great journey so far but<br/>there is so much I still don't know which makes me excited...</p>
+            <p>Hey! I am Madhusudan and I am pursuing my B.Tech Degree from VIT in IT. I have been since the beginning of my time here on earth, fascinated by these machines<br/>that we call computers, and from understanding what the internet is and how it works to now learning to make my own websites is a great journey so far but<br/>there is so much I still don't know which makes me excited...</p>
           </div>
           <div className="skills">
             <h3><u>Skills:</u></h3>
